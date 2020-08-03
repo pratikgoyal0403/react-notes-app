8bef2118107dc09a853a48dddee0f133a70c040d
pratik goyal <goyal.pratik0304@gmail.com>
2020-08-03 16:07:22 +0530

    fixed button

--- src/components/UI/button/button.js
+++ src/components/UI/button/button.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React from "react";
+import classes from "./button.module.css";
 
-const button = props => {
-return <button onClick={props.clicked}>{props.children}</button>;
-}
+const button = (props) => {
+  return (
+    <button onClick={props.clicked} className={classes.button}>
+      {props.children}
+    </button>
+  );
+};
 
 export default button;
